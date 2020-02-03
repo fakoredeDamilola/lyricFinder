8bf261ef4f78f77e326d218284d225ca262ffa22
fakoredeDamilola <dfakorede432@stu.ui.edu.ng>
2020-02-03 20:53:37 +0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,112 @@
+export default [
+    {
+        id:"1",
+        slug:'imagine-dragons',
+        name:"imagine dragons",
+        track:"natural",
+        album:"natural",
+        featured:"yes",
+        genre:"Alternative",
+        words:"No",
+        lyrics:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ex, deserunt harum reprehenderit aliquam cumque corporis facere! Perspiciatis eveniet ducimus est. Neque praesentium quo est ipsam, ipsum doloremque deleniti tenetur in vitae voluptatibus vel, provident, odio autem distinctio cupiditate fugiat nesciunt sint porro quidem recusandae itaque saepe. Nam error, unde quae ipsa saepe veritatis laboriosam nostrum pariatur a voluptatibus distinctio dolores, soluta quisquam amet. Quasi consequuntur molestiae perferendis id numquam maxime quos rerum praesentium nihil tempora minima, nobis perspiciatis at?"
+    },
+    {
+        id:"2",
+        slug:'post-malone',
+        name:"Post Malone",
+        track:"Better Now",
+        album:"beerbongs & bentley",
+        featured:"no",
+        genre:"Alternative",
+        words:"yes",
+        lyrics:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ex, deserunt harum reprehenderit aliquam cumque corporis facere! Perspiciatis eveniet ducimus est. Neque praesentium quo est ipsam, ipsum doloremque deleniti tenetur in vitae voluptatibus vel, provident, odio autem distinctio cupiditate fugiat nesciunt sint porro quidem recusandae itaque saepe. Nam error, unde quae ipsa saepe veritatis laboriosam nostrum pariatur a voluptatibus distinctio dolores, soluta quisquam amet. Quasi consequuntur molestiae perferendis id numquam maxime quos rerum praesentium nihil tempora minima, nobis perspiciatis at?"
+    },
+    {
+        id:"3",
+        slug:"captain",
+        name:"captain",
+        track:"hillsong",
+        album:"hillsong",
+        featured:"yes",
+        genre:"Alternative",
+        words:"yes",
+        lyrics:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ex, deserunt harum reprehenderit aliquam cumque corporis facere! Perspiciatis eveniet ducimus est. Neque praesentium quo est ipsam, ipsum doloremque deleniti tenetur in vitae voluptatibus vel, provident, odio autem distinctio cupiditate fugiat nesciunt sint porro quidem recusandae itaque saepe. Nam error, unde quae ipsa saepe veritatis laboriosam nostrum pariatur a voluptatibus distinctio dolores, soluta quisquam amet. Quasi consequuntur molestiae perferendis id numquam maxime quos rerum praesentium nihil tempora minima, nobis perspiciatis at?"
+    },
+    {
+        id:"4",
+        slug:"holy-spirit",
+        name:"holy spirit",
+        track:"koinonia",
+        album:"koinonia",
+        featured:"yes",
+        genre:"sermon",
+        words:"No",
+        lyrics:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ex, deserunt harum reprehenderit aliquam cumque corporis facere! Perspiciatis eveniet ducimus est. Neque praesentium quo est ipsam, ipsum doloremque deleniti tenetur in vitae voluptatibus vel, provident, odio autem distinctio cupiditate fugiat nesciunt sint porro quidem recusandae itaque saepe. Nam error, unde quae ipsa saepe veritatis laboriosam nostrum pariatur a voluptatibus distinctio dolores, soluta quisquam amet. Quasi consequuntur molestiae perferendis id numquam maxime quos rerum praesentium nihil tempora minima, nobis perspiciatis at?"
+    },
+    {
+        id:"5",
+        slug:"forever-reign",
+        name:"Forever Reign",
+        track:"Koinonia",
+        album:"Koinonia",
+        featured:"yes",
+        genre:"sermon",
+        words:"No",
+        lyrics:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ex, deserunt harum reprehenderit aliquam cumque corporis facere! Perspiciatis eveniet ducimus est. Neque praesentium quo est ipsam, ipsum doloremque deleniti tenetur in vitae voluptatibus vel, provident, odio autem distinctio cupiditate fugiat nesciunt sint porro quidem recusandae itaque "
+    },
+    {
+        id:"6",
+        slug:"dragons",
+        name:"dragons",
+        track:"nat",
+        album:"nat",
+        featured:"no",
+        genre:"Altern",
+        words:"No",
+        lyrics:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ex, deserunt harum reprehenderit aliquam cumque corporis facere! Perspiciatis eveniet ducimus est. Neque praesentium quo est ipsam, ipsum doloremque deleniti tenetur in vitae voluptatibus vel, provident, odio autem distinctio cupiditate fugiat nesciunt sint porro quidem recusandae itaque saepe. Nam error, unde quae ipsa saepe veritatis laboriosam nostrum pariatur a voluptatibus distinctio dolores, soluta quisquam amet. Quasi consequuntur molestiae perferendis id numquam maxime quos rerum praesentium nihil tempora minima, nobis perspiciatis at?"
+    },
+    {
+        id:"7",
+        slug:"imagine",
+        name:"imagine",
+        track:"vision",
+        album:"natural",
+        featured:"no",
+        genre:"Alter",
+        words:"No",
+        lyrics:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ex, deserunt harum reprehenderit aliquam cumque corporis facere! Perspiciatis eveniet ducimus est. Neque praesentium quo est ipsam, ipsum doloremque deleniti tenetur in vitae voluptatibus vel, provident, distinctio dolores, soluta quisquam amet. Quasi consequuntur molestiae perferendis id numquam maxime quos rerum praesentium nihil tempora minima, nobis perspiciatis at?"
+    },
+    {
+        id:"8",
+        slug:"seeing",
+        name:"seeing",
+        track:"i like it",
+        album:"i like it",
+        featured:"no",
+        genre:"girl like you",
+        words:"No",
+        lyrics:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ex, deserunt harum reprehenderit aliquam cumque corporis facere! Perspiciatis eveniet ducimus est. Neque praesentium quo est ipsam, ipsum doloremque deleniti tenetur in vitae voluptatibus vel,cupiditate fugiat nesciunt sint porro quidem recusandae itaque saepe. Nam error, unde quae ipsa saepe veritatis laboriosam nostrum pariatur a voluptatibus distinctio dolores, soluta quisquam amet. Quasi consequuntur molestiae perferendis id numquam maxime quos rerum praesentium nihil tempora minima, nobis perspiciatis at?"
+    },
+    {
+        id:"9",
+        slug:"you",
+        name:"you",
+        track:"yo",
+        album:"yo",
+        featured:"no",
+        genre:"Alt",
+        words:"No",
+        lyrics:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ex, deserunt harum reprehenderit aliquam cumque corporis facere! Perspiciatis eveniet ducimus est. Neque praesentium quo est ipsam, ipsum doloremque deleniti tenetur in vitae voluptatibus vel, provident, odio autem distinctio cupiditate fugiat nesciunt sint porro quidem recusandae itaque saepe. Nam error, unde quae ipsa saepe veritatis laboriosam nostrum pariatur a voluptatibus distinctio dolores, soluta quisquam amet. Quasi consequuntur molestiae perferendis id numquam maxime quos rerum praesentium nihil tempora minima, nobis perspiciatis at?"
+    },
+    {
+        id:"10",
+        slug:"you-are",
+        name:"you are",
+        track:"nat",
+        album:"nat",
+        featured:"yes",
+        genre:"Alt",
+        words:"No",
+        lyrics:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ex, deserunt harum reprehenderit aliquam cumque corporis facere! Perspiciatis eveniet ducimus est. Neque praesentium quo est ipsam, ipsum doloremque deleniti tenetur in vitae voluptatibus vel, provident, odio autem distinctio cupiditate fugiat nesciunt sint porro quidem recusandae itaque saepe. Nam error, unde quae ipsa saepe veritatis laboriosam nostrum pariatur a voluptatibus distinctio dolores, soluta quisquam amet. Quasi consequuntur molestiae perferendis id numquam maxime quos rerum praesentium nihil tempora minima, nobis perspiciatis at?"
+    }
+]
